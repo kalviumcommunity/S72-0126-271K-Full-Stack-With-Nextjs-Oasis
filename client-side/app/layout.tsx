96a527b8be55f1a6a227f2a89eba7a98a059dff9
Anant <sastrulaanant@gmail.com>
2026-01-27 15:14:22 +0530

    added the offline working website feature

--- client-side/app/layout.tsx
+++ client-side/app/layout.tsx
@@ -29,6 +29,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.webmanifest" />
+        <meta name="theme-color" content="#0ea5e9" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
